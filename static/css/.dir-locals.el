d300d11bdfc07925ddf96faad7b874e7f3ad6607
jiacai2050 <dev@liujiacai.net>
2025-09-14 08:27:16 +0800

    custom footer css

--- static/css/.dir-locals.el
+++ static/css/.dir-locals.el
@@ -1,4 +1,4 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((css-mode . ((css-indent-offset . 2))))
+((css-mode . ((css-indent-offset . 4))))
